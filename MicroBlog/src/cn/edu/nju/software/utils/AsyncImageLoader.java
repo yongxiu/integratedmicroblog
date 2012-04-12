cc98c93a91a8eab9841f58a43d28cb86b8389a3b
yongxiu <cuiyongxiu@gmail.com>
2012-04-12 11:52:50 +0000

    Edit the AsyncImageLoader, add a thread pool

--- MicroBlog/src/cn/edu/nju/software/utils/AsyncImageLoader.java
+++ MicroBlog/src/cn/edu/nju/software/utils/AsyncImageLoader.java
@@ -6,20 +6,19 @@ import java.lang.ref.SoftReference;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.widget.ImageView;
 
 public class AsyncImageLoader {
 
-	private ConcurrentHashMap<String, SoftReference<Drawable>> imageCache;
-
-	public AsyncImageLoader() {
-		imageCache = new ConcurrentHashMap<String, SoftReference<Drawable>>();
-	}
+	private static ConcurrentHashMap<String, SoftReference<Drawable>> imageCache 
+		= new ConcurrentHashMap<String, SoftReference<Drawable>>();
+	private static ExecutorService executorService = Executors.newFixedThreadPool(5);    //固定五个线程来执行任务
 
 	public void loadDrawable(final String imageUrl, final ImageView imageView) {
 		if (imageCache.containsKey(imageUrl)) {
@@ -35,15 +34,14 @@ public class AsyncImageLoader {
 				imageView.setImageDrawable((Drawable) message.obj);
 			}
 		};
-		new Thread() {
-			@Override
-			public void run() {
+		executorService.submit(new Runnable() {
+            public void run() {
 				Drawable drawable = loadImageFromUrl(imageUrl);
 				imageCache.put(imageUrl, new SoftReference<Drawable>(drawable));
 				Message message = handler.obtainMessage(0, drawable);
 				handler.sendMessage(message);
 			}
-		}.start();
+		});
 	}
 
 	public static Drawable loadImageFromUrl(String url) {
